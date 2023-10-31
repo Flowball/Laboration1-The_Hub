83f4e20c11832df87fd2dcf7aafe7ac2f8308563
oscar.jigbring <oscar.jigbring@hotmail.se>
2023-10-31 15:04:07 +0100

    Added function to render room depending on click

--- script/script.js
+++ script/script.js
@@ -8,10 +8,20 @@ const roomBtns = document.getElementsByClassName("roomBtn");
 for (btn of roomBtns) {
   btn.addEventListener("click", (e) => {
     playerState.roomNr = e.target.id;
-    // collectItemToInv("nubben");
     console.log(playerState.roomNr);
+    renderRoom(e.target.id);
   });
 }
+
+// RENDER ROOM!!!
+function renderRoom(target) {
+  if (playerState.roomNr == target) {
+    const targetRoom = target;
+    const targetDiv = document.getElementById(targetRoom);
+    targetDiv.classList.add("hide");
+  }
+}
+// RENDER ROOM!!!
 
 // INVENTORY & COLLECT
 const inventory = [];
@@ -26,6 +36,9 @@ function collectItemToInv(itemName) {
     notification(`${itemName.toUpperCase()} ALREADY IN INVENTORY`);
   } else {
     inventory.push(itemName);
+    const itemId = itemName;
+    const itemDiv = document.getElementById(itemId);
+    itemDiv.classList.add("hide");
     div.textContent = itemName;
     div.id = "invItem";
     console.log(`ITEM PICKED UP: ${itemName}`);
@@ -55,3 +68,33 @@ inventoryBtn.addEventListener("click", () => {
   inventoryBox.classList.toggle("hideMenu");
 });
 //SHOW INVENTORY
+
+//burger menu toggle
+const burger = document.querySelector("#burger");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector("#closeMenu");
+
+burger.addEventListener("click", () => {
+  modal.classList.toggle("showModal");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.toggle("showModal");
+});
+
+//RESET FUNCTION
+
+function resetAll() {
+  const playerState = {
+    roomNr: 0,
+    inventorySlots: 5,
+  };
+  const inventory = [];
+  location.reload();
+}
+
+//reroute to new site function
+
+function reRoute(siteValue) {
+  window.location.href = siteValue;
+}
