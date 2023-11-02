48eee6f707c23f99e1c67fded1675e79a970e4c2
oscar.jigbring <oscar.jigbring@hotmail.se>
2023-11-02 14:18:30 +0100

    Added changes, aswell as music quiz function

---
+++ script/scenes.js
@@ -0,0 +1,24 @@
+const scenes = [
+  {
+    id: 0,
+    text: "Välkommen till musikquis!!",
+    itemToPickup: "knife",
+    subText:
+      "🎵Slå på ljud🎵 starta genom att klicka på PLAY, skriv sedan din gissning! ( slå enter )",
+  },
+  {
+    id: 1,
+    text: "Välkommen till MÅLNINGEN",
+    itemToPickup: "ball",
+  },
+  {
+    id: 2,
+    text: "Välkommen till DJÄVULEN",
+    itemToPickup: "hat",
+  },
+  {
+    id: 3,
+    text: "Välkommen till YOOOOGA",
+    itemToPickup: "clock",
+  },
+];
--- script/script.js
+++ script/script.js
@@ -9,21 +9,50 @@ for (btn of roomBtns) {
   btn.addEventListener("click", (e) => {
     playerState.roomNr = e.target.id;
     console.log(playerState.roomNr);
-
     renderRoom(e.target.id);
   });
-}
-// LÄGG TILL LOGIK FÖR ATT RENDERA , KANSKE LÄGGA TILL ATT MAN BHEÖVER FYLLA I NAMN INNAN ALLT RENDERAS?
-// FRAMFÖRALLT LÄGG TILL SÅ ATT SIDA RENDERAS BEROENDE PÅ roomNr
-if (playerState.roomNr == "room1") {
 }
 
 // RENDER ROOM!!!
 function renderRoom(target) {
   if (playerState.roomNr == target) {
     const targetRoom = target;
-    const targetDiv = document.getElementById(targetRoom);
-    targetDiv.classList.remove("hide");
+    const pickedRoom = document.querySelector(`#${target}`);
+
+    const playRoom = document.querySelector("#playRoom");
+    const roomText = document.querySelector("#roomText");
+    const roomSubtext = document.querySelector("#roomSubText");
+    const itemToPickup = document.querySelector(".itemToPickup");
+    const musicHolder = document.querySelector(".musicHolder");
+
+    switch (pickedRoom) {
+      case room1:
+        roomPicker.classList.add("hide");
+        playRoom.classList.remove("hide");
+        console.log("DU VALDE RUM1!");
+        musicHolder.classList.toggle("hide");
+        roomText.innerHTML = scenes[0].text;
+        roomSubtext.innerHTML = scenes[0].subText;
+        break;
+      case room2:
+        roomPicker.classList.add("hide");
+        playRoom.classList.remove("hide");
+        roomText.innerHTML = scenes[1].text;
+        console.log("DU VALDE RUM2!");
+        break;
+      case room3:
+        roomPicker.classList.add("hide");
+        playRoom.classList.remove("hide");
+        roomText.innerHTML = scenes[2].text;
+        console.log("DU VALDE RUM3!");
+        break;
+      case room4:
+        roomPicker.classList.add("hide");
+        playRoom.classList.remove("hide");
+        roomText.innerHTML = scenes[3].text;
+        console.log("DU VALDE RUM4!");
+        break;
+    }
   }
 }
 // RENDER ROOM!!!
@@ -104,3 +133,56 @@ function resetAll() {
 function reRoute(siteValue) {
   window.location.href = siteValue;
 }
+
+//MUSIC QUIZ
+const playBtn = document.querySelector("#musicBtn-1");
+const response = document.querySelector("#response");
+const secondResponse = document.querySelector("#secondResponse");
+const guessInput = document.querySelector("#guessInput");
+let songIndex = 0;
+
+playBtn.addEventListener("click", startMusicQuiz);
+console.log("songidex =" + songIndex);
+function startMusicQuiz() {
+  let songSource = someMusic[`${songIndex}`].source;
+  console.log("songidex =" + songIndex);
+  secondResponse.textContent = "";
+  console.log(songSource);
+  playBtn.textContent = "PLAY";
+  playMusic(songSource);
+  let songTitle = someMusic[`${songIndex}`].songTitle;
+  guessInput.classList.remove("hide");
+  response.textContent = someMusic[`${songIndex}`].question;
+  guessInput.addEventListener("change", () => {
+    if (guessInput.value.toUpperCase() === someMusic[`${songIndex}`].correct) {
+      console.log("songidex =" + songIndex);
+      console.log("YEA!!!");
+      secondResponse.textContent = `korrekt svar!`;
+      playBtn.textContent = "Klicka för att spela nästa";
+      playBtn.addEventListener("click", () => {
+        songIndex++;
+        startMusicQuiz();
+      });
+    } else {
+      console.log("songidex =" + songIndex);
+      console.log("fel svar!!!!!");
+      secondResponse.textContent = `fel svar... prova igen!`;
+    }
+  });
+}
+
+function guess() {
+  if (guessInput.value === "hej") {
+    console.log("YEHOW");
+  } else {
+    console.log("super fail...");
+  }
+}
+function playMusic(songSource) {
+  var audio = new Audio(songSource);
+  audio.play();
+}
+// function playError() {
+//   var audio = new Audio("audio/error.mp3");
+//   audio.play();
+// }
