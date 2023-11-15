6b1276fbc99c24271540c31cfdcbd3995fb624aa
oscar.jigbring <oscar.jigbring@hotmail.se>
2023-11-15 13:25:29 +0100

    removed dicegame, added painting files

--- scripts/painting.js
+++ scripts/painting.js
@@ -1,3 +1,26 @@
-const roomSubtext = document.querySelector("#roomSubText");
-const diceBtn = document.createElement("button");
-const container = document.querySelector("#container");
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container");
+const searchInput = document.querySelector("[data-search");
+
+let users = [];
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  users.forEach((user) => {
+    const isVisible = user.title.toLowerCase().includes(value);
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
+
+fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+  res.json().then((data) => {
+    users = data.map((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = user.title;
+      body.textContent = user.completed;
+      userCardContainer.append(card);
+      return { title: user.title, completed: user.completed, element: card };
+    });
+  })
+);
